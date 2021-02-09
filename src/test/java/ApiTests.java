5e0681a8af739b9b3b29d3dbd78e63ef7f13eb19
lordbarrington <quantas@gmail.com>
2021-02-09 15:03:33 +0300

    Some new tests are added

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,13 +1,57 @@
+import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class ApiTests {
 
     @Test
-    public void statusCodeTest(){
+    public void getStatusCodeTest() throws IOException {
         CloseableHttpResponse closeableHttpResponse = RestClient.getRequest("https://reqres.in/api/users?page=2");
         int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
         System.out.println(statusCode);
     }
+
+    @Test
+    public void assertTest() throws IOException {
+        CloseableHttpResponse closeableHttpResponse = RestClient.getRequest("https://reqres.in/api/users?page=2");
+        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+        Assert.assertEquals(statusCode,200);
+    }
+
+    @Test
+    public void jsonResponseTest() throws IOException {
+        CloseableHttpResponse closeableHttpResponse = RestClient.getRequest("https://reqres.in/api/users?page=2");
+        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        JSONObject responseJson = new JSONObject(responseString);
+        System.out.println(responseJson);
+    }
+
+    @Test
+    public void headersTest() throws IOException {
+        CloseableHttpResponse closeableHttpResponse = RestClient.getRequest("https://reqres.in/api/users?page=2");
+        Header[] headersArray = closeableHttpResponse.getAllHeaders();
+        HashMap<String, String> allHeaders = new HashMap<String, String>();
+        for(Header header : headersArray){
+            allHeaders.put(header.getName(),header.getValue());
+        }
+        System.out.println("Headers Array-->" + allHeaders);
+    }
+
+    @Test
+    public void jsonExtractTest() throws IOException {
+        CloseableHttpResponse closeableHttpResponse = RestClient.getRequest("https://reqres.in/api/users?page=2");
+        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        JSONObject responseJson = new JSONObject(responseString);
+        String value = RestClient.getValueByJPath(responseJson, "page");
+        System.out.println("Page value:-->" + value);
+    }
+
+
 
 }
--- src/test/java/RestClient.java
+++ src/test/java/RestClient.java
@@ -1,19 +1,41 @@
 import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 public class RestClient {
 
-    public static CloseableHttpResponse getRequest (String url) {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault();){
-        HttpGet httpget = new HttpGet(url); //http get request (create get connection with particular url)
+    public static CloseableHttpResponse getRequest (String url) throws IOException {
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpget = new HttpGet(url); //http get request (create get connection with particular url)
             return httpClient.execute(httpget);
-    } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
     }
+
+    public static String getValueByJPath(JSONObject jsonResponse, String jsonPath){
+        Object obj = jsonResponse;
+        for(String s : jsonPath.split("/"))
+            if(!s.isEmpty())
+                if(!(s.contains("[") || s.contains("]")))
+                    obj = ((JSONObject) obj).get(s);
+                else if(s.contains("[") || s.contains("]"))
+                    obj = ((JSONArray) ((JSONObject) obj).get(s.split("\\[")[0])).get(Integer.parseInt(s.split("\\[")[1].replace("]", "")));
+        return obj.toString();
+    }
+
+
+
+
+
+
+
+
+
 }
